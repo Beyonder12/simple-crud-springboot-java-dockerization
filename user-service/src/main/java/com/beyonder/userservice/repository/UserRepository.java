5433f81c768d10bf0248105c83f967306a9342a8
Beyonder12 <fajri.illahi1211@gmail.com>
2022-12-06 15:36:45 +0700

    Beyonder : crud

---
+++ user-service/src/main/java/com/beyonder/userservice/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.beyonder.userservice.repository;
+
+import com.hkdemircan.demo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    User findFirstById(Long id);
+}
